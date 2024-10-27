b993c46dc3e86aec001174380e07b57cee76453d
Pedro Lourega <pedrolourega1@gmail.com>
2024-10-27 17:15:48 -0300

    Create test1.py

---
+++ simplex_exercise/test1.py
@@ -0,0 +1,4 @@
+nome = input("Qual seu nome? ")#Cria uma variavel nome e pede para o usuário digitar seu nome para alocar valor nela.
+#Create a variable name and ask the user to enter its name to allocate value to it.
+
+print(f"Seu nome é {nome}")#Printa o nome escrito pelo usuário juntamente com o valor da variavel entre {}, utilizando print(f).
