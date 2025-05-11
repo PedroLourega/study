d7c72cf32ea0102e2ae5104171fad158e87c6637
Pedro Lourega <pedrolourega1@gmail.com>
2025-05-10 21:35:46 -0300

    update
    Aula1

--- studies_new/course/aula1.py
+++ studies_new/course/aula1.py
@@ -1,1 +1,14 @@
-print('Hello World!')
+  # Permite escrever comentário
+print(123) # Na frente
+# Em baixo
+
+
+"""
+
+Docstring - Para escrever em mais de uma linha
+
+"""
+
+'''
+Aspas simples também funcionam
+'''
