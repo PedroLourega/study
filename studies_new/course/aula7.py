29a19ef459a6d17b1021e4d0b4987dea31846536
Pedro <pedrolourega1@gmail.com>
2025-05-15 20:42:03 -0300

    update9

---
+++ studies_new/course/aula7.py
@@ -0,0 +1,17 @@
+#Variáveis são usada para salvar algo na memória do PC
+#PEP8: inicie variais com letras minusculas, pode usar numeros e underline(_)
+#O sinal de = é um operador de atribuição. Ele é usado para atribuir um valor a um nome(variável)
+#Uso: nome_variavel = expressão
+
+nome_completo = "Pedro Henrique Lourega"
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois) # Pedro Henrique Lourega, 4
+print(soma_dois_mais_dois) # 4
+num_int = int("1")
+print(num_int) 
+
+nome = 'Pedro'
+idade = 22
+maior_de_idade = bool(idade >= 18)
+print('Nome: ', nome, "idade: ", idade)
+print('É maior? ', maior_de_idade)
---
+++ studies_new/course/aula8.py
@@ -0,0 +1,14 @@
+nome = 'Pedro Henrique'
+sobrenome = 'Lourega'
+idade = 22
+ano_nascimento = (2025 - idade)
+maior_de_idade = bool(idade >= 18)
+altura_metros = float(1.74)
+
+
+print('Nome:', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros: ', altura_metros)
