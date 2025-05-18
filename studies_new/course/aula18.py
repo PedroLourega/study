b10434aec37af068d11727e206e1c6ee711ef969
Pedro <pedrolourega1@gmail.com>
2025-05-17 21:36:56 -0300

    Create aula18.py

---
+++ studies_new/course/aula18.py
@@ -0,0 +1,24 @@
+"""
+
+Operadores de comparação(relacionais)
+
+> - Maior - 2 > 1
+>= - Maior ou igual - 2 >= 2
+< - Mmenor - 1 < 2
+<= - Menor ou igual - 2 <= 2
+== - igual - "a" == "a"
+!= - diferente - "a" != "b"
+
+"""
+
+#comando para shell interativo python:
+#python -i '/c:\Users\Escola MVP\Documents\Local\structure_code_python\studies_new\course\aula18.py'
+
+maior = 2 > 2 #False
+maior_ou_igual = 2 >= 2 #True
+menor = 1 < 2 #True
+menor_ou_igual = 2 <= 2 #True
+igual = 'a' == 'a' #True
+diferente = 'a' != 'n' #True
+
+print(diferente) 
