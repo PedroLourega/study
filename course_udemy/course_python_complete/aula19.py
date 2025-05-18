44b1eb8b816ba6f1222a8d9a744470593696c3e9
Pedro <pedrolourega1@gmail.com>
2025-05-18 19:10:38 -0300

    update14

---
+++ course_udemy/course_python_complete/aula19.py
@@ -0,0 +1,7 @@
+num1 = input("Digite o primeiro valor:")
+num2 = input("Digite o segundo valor: ")
+
+if num1 >= num2:
+    print(f"O primeiro valor:{num1} é maior ou igual o segundo valor:{num2}!")
+else:
+    print(f"O segundo valor:{num2} é maior ou igual o primeiro valor:{num1}")
---
+++ course_udemy/course_python_complete/aula20.py
@@ -0,0 +1,21 @@
+#Operadores lógicos
+
+# and(e) | or(ou) | not(não)
+
+#and -> Todas as condições precisam ser True.
+
+#OBS: Se qualquer valor for considerado falso, a expressão inteira será avalidade naquele valor.
+
+#São considerados falsy: 0|0.0|''|False
+
+
+#Também existe o tipo None que é usado para representar um "Não Valor".
+
+entrada = input("[E]Entrar [S]Sair:")
+senha_digitada = input("Senha:")
+senha_permitida = "123456"
+
+if entrada == "E" and senha_digitada == senha_permitida:
+    print("Entrar")
+else:   
+    print("Sair")
