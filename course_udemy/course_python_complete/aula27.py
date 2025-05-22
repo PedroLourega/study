6d3c926db2ae7ffb98934dcddbf868d1929736ab
Pedro <pedrolourega1@gmail.com>
2025-05-21 21:32:31 -0300

    update18

--- course_udemy/course_python_complete/aula27.py
+++ course_udemy/course_python_complete/aula27.py
@@ -14,16 +14,20 @@ Se nada for digitado em nome ou idade:
     exiba "Desculpe, você deixou campos vazios."
 """
 
-nome =input("Digite seu nome: ")
-idade = int(input("Digite a sua idade: "))
-nome_invertido = nome[-1::-1]
-tem_espaco = " " in nome
+nome = input("Digite seu nome: ")
+idade = input("Digite a sua idade: ")
 
 if nome and idade:
     print(f"Seu nome é {nome}, é realmente muito bonito!")
-    print(f"Seu nome invertido é: {nome_invertido}")
-    print(f"Seu nome contém espaços: {tem_espaco}")
-    print(f"A primeira letra do seu nome é {nome[0]}")
+    print(f"Seu nome invertido é: {nome[::-1]}")
+
+    if " " in nome:
+        print("Seu nome contém espaços!")
+    else:
+        print("Seu nome NÃO contém espaços!")
+
+    print(f"Seu nome tem {len(nome)} letras.")
+    print(f"A primeira letra do seu nome é: {nome[0]}")
     print(f"A ultima letra do seu nome é: {nome[-1]}")
 
 else:
