3d6d37ef4a3d1ab665cd80325a379859476dc981
Pedro <pedrolourega1@gmail.com>
2025-06-02 17:40:08 -0300

    curso_santander_add

---
+++ course_santander_academy/course_python_introduction/aula1.py
@@ -0,0 +1,45 @@
+print("Olá mundo!")
+
+#Identação 
+"""
+if condition:
+
+    # Bloco de código se a condição for verdadeira
+
+    instrucao1
+    instrucao2
+
+else:
+
+    # Bloco de código se a condição for falsa
+
+    instrucao3
+    instrucao4
+"""
+
+#Comentários 
+
+# Este é um comentário de uma única linha
+
+"""
+Este é um comentário
+de várias linhas
+"""
+
+
+#Sensibilidade a maiúsculas e minusculas
+#Python distingue entre maiúsculas e minúsculas. Portanto, variável, Variável e VARIÁVEL são consideradas variáveis diferentes.
+
+ 
+ #Ponto e vírgula
+ #Diferente de outras linguagens, o Python não requer o uso de ponto e vírgula(;) ao final de cada instrução. No entanto, se você desejar escrever várias instruições em uma única linha, pode separá-las com um ponto e vírgula.
+
+ #ex: instrucao1; instrucao2; instrucao3
+
+ 
+ #Uso de Parênteses
+ #os parenteses são utlizados para agrupar espressões, deifnir funções e realizar chamadas a funções.
+ 
+ #resultado = (a +b) * c 
+
+ 
---
+++ course_santander_academy/course_python_introduction/aula2.py
@@ -0,0 +1,37 @@
+"""
+#Tipos de dados básicos
+
+Inteiros(int) 
+ Os números inteiros são aqueles que não têm parte decimal. Em Python, são representados simplesmente escrevendo o número sem aspas nem pontos decimais. Por exemplo:
+
+    idade = 25
+    quantidade = 100
+
+---
+
+Flutuantes(float)
+são aqueles que têm uma parte decimal. Em Python, são representados utilizando um ponto para separar a parte inteira da parte decimal. Por exemplo:
+
+    preço = 9.99
+    altura = 1.75
+
+---
+
+Cadeias de texto(strings)
+As cadeias de texto, ou simplesmente cadeias, são sequências de caracteres encerradas entre aspas simples ('...') ou duplas ("..."). São utilizadas para representar texto em Python. Por exemplo:
+
+nome = "Juan"
+mensagem = '¡Hola, mundo!'
+
+#Você pode incluir caracteres especiais nas cadeias utilizando o caractere de escape \. Por exemplo, para incluir aspas dentro de uma cadeia, você pode usar \' ou \". Também pode utilizar a notação de tripla aspa
+
+---
+Booleanos
+Os valores booleanos representam os valores de verdade: True (verdadeiro) e False (falso). São comumente utilizados em expressões condicionais e operações lógicas. Por exemplo:
+
+é_maior_de_idade = True
+tem_desconto = False
+
+#Os valores booleanos em Python começam com uma letra maiúscula: True e False.
+
+"""
