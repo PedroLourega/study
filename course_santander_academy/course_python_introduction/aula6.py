d842aade37d6870b417f56c5d71304c4b999f27f
Pedro Lourega <pedrolourega1@gmail.com>
2025-06-03 18:19:43 -0300

    update2_santander

---
+++ course_santander_academy/course_python_introduction/aula6.py
@@ -0,0 +1,20 @@
+"""
+##LOOPS
+Os loops nos permitem repetir um bloco de código várias vezes. Em Python, os loops mais comuns são for e while.
+
+### FOR
+O loop for é utilizado para iterar sobre uma sequência(como uma lista, uma tupla ou uma string) ou qualquer objeto iterável. A sintaxe básica é a seguinte:
+
+for variável in sequência:
+    #bloco de código que vai repetir
+ex:
+
+frutas = ["maça", "banana", "laranja"]
+
+for fruta in frutas:
+    print(fruta)
+
+Neste exemplo, o loop for itera sobre a lista frutas. Em cada iteração, a variável fruta assume o valor de um elemento da lista, e o bloco de código dentro do loop é executado. Neste caso, cada fruta é impressa em uma linha separada.
+
+"""
+
