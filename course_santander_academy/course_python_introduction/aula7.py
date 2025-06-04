dcfee0b6ef5709210f8747d287363344a0ae1537
Pedro <pedrolourega1@gmail.com>
2025-06-03 21:43:11 -0300

    update3_santander

---
+++ course_santander_academy/course_python_introduction/aula7.py
@@ -0,0 +1,14 @@
+"""
+# ESTRUTURAS DE DADOS
+
+As estruturas de dados nos permitem organizar e armazenar dados de maneira eficiente em nossos programas. Python fornece várias estruturas de dados integradas, como listas, tuplas, dicionários e conjuntos, cada uma com suas próprias características e usos.
+
+
+## Listas
+
+Uma lista é uma estrutura de dados mutável e ordenada que permite armazenar uma coleção de elementos. Os elementos de uma lista podem ser de diferentes tipos de dados e são encerrados entre colchetes [], separados por vírgulas.
+
+ 
+
+
+"""
