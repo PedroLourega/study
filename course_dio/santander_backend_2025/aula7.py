0ed97e5959ac6a4013a486119243db8ac0ec944c
Pedro <pedrolourega1@gmail.com>
2025-06-25 21:38:01 -0300

    aula2_update3
    Entradas e Saídas de dados.

---
+++ course_dio/santander_backend_2025/aula7.py
@@ -0,0 +1,21 @@
+# Numerico para String
+
+preco = 10.50
+idade = 28 
+
+print(str(preco))
+
+print(str(idade))
+
+texto = f"idade {idade} preco {preco}"
+print(texto)
+
+
+# String para numero
+
+preco = "10.50"
+idade = "28"
+
+print(float(preco))
+
+print(int(idade))
---
+++ course_dio/santander_backend_2025/aula8.py
@@ -0,0 +1,6 @@
+nome = "Pedro"
+sobrenome = "Lourega"
+
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n") #função end="" modifica o final / (\n quebra a linha)
+print(nome, sobrenome, sep="#")# função sep, muda a separação
