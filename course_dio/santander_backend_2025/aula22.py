73b2ef08aad5bcaeb3d822a0595f6c7e139867ab
Pedro <pedrolourega1@gmail.com>
2025-07-18 18:12:41 -0300

    aulas6_update7

---
+++ course_dio/santander_backend_2025/aula22.py
@@ -0,0 +1,33 @@
+#Multiplas strings
+
+
+nome = "Pedro"
+
+mensagem = f"""
+Olá meu nom é {nome},
+Eu estou aprendendo Python
+"""
+
+print(mensagem)
+
+mensagem2 = f"""
+    Olá meu nom é {nome},
+Eu estou aprendendo Python.
+        Estou aprendendo a utilizar os recuos.
+"""
+
+print(mensagem2)
+
+
+print(
+    """
+    =============== MENU ===============
+
+    1 - DEPOSITAR
+    2 - SACAR
+    0 - SAIR
+
+    =====================================
+
+    """
+)
