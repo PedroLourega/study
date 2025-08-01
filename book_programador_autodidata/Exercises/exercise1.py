2ed1770adea81a7e05193d64f8b2df10bff42eab
Pedro <pedrolourega1@gmail.com>
2025-08-01 12:10:48 -0300

    exercises1_and_2
    Exercises 1 and 2 completed

---
+++ book_programador_autodidata/Exercises/exercise1.py
@@ -0,0 +1,2 @@
+print("Hello World!")
+print('Olá mundo, esta seção do repositório é destinada para a realização de exercicios do livro: \n "Programador Autodidata"')
---
+++ book_programador_autodidata/Exercises/exercise2.py
@@ -0,0 +1,13 @@
+# EXERCICIO DE CONTAR 100X
+
+count = 0
+
+for i in range(100):
+    count += 1
+    print("Hello World!")
+    print(f"Contagem número:{count}")
+
+
+# é possivel separar linhas de código com a barra invertida(\)
+print\
+    ("Teste")
