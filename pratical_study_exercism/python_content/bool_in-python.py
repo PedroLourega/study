425aabf81957515eccf5dfc133601b02aeb67b5a
Pedro Lourega <pedrolourega1@gmail.com>
2026-02-02 19:51:44 -0300

    bool_in_python

---
+++ pratical_study_exercism/python_content/bool_in-python.py
@@ -0,0 +1,15 @@
+# Python represents true and false values with the bool type, which is a subtype of int. There are only two values in this type: True and False. These values can be bound to a variable:
+
+true_variable = True
+false_variable = False
+
+# We can evaluate Boolean expressions using the and, or, and not operators:
+
+true_variable = True and True
+false_variable = True and False
+
+true_variable = False or True
+false_variable = False or False
+
+true_variable = not False
+false_variable = not True
